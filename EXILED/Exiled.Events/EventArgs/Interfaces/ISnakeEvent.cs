5f916c329422c3fe363b70df142e5913459fc651
Lumi <me@jxtq.moe>
2025-05-26 11:32:41 +0100

    fix: no copyright header

--- EXILED/Exiled.Events/EventArgs/Interfaces/ISnakeEvent.cs
+++ EXILED/Exiled.Events/EventArgs/Interfaces/ISnakeEvent.cs
@@ -1,4 +1,11 @@
-﻿namespace Exiled.Events.EventArgs.Interfaces
+﻿// -----------------------------------------------------------------------
+// <copyright file="ISnakeEvent.cs" company="ExMod Team">
+// Copyright (c) ExMod Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.EventArgs.Interfaces
 {
     using Exiled.API.Features.Items;
     using InventorySystem.Items.Keycards.Snake;
